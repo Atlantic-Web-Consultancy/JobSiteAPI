2f8d07e4be67ff6734e981484968cf81e6fc355d
Samuel Gasser <sgasser94@gmail.com>
2021-05-25 13:51:16 -0700

    Finished Get /employers/jobpostings route

--- controllers/employer/getEmployerPostings.js
+++ controllers/employer/getEmployerPostings.js
@@ -1,16 +1,17 @@
 const Model = require('../../model/');
 
-const getEmployerPostings = (data, callback) => {
-  if (Object.keys(data).length === 7) {
-    Model.Employer.getEmployerPostings(data, (err) => {
+const getEmployerPostings = (req, callback) => {
+  var jobSiteCookie = req.cookies.jobsite;
+  if (jobSiteCookie) {
+    Model.Employer.getEmployerPostings(jobSiteCookie, (err, data) => {
       if (err) {
         callback(err);
       } else {
-        callback(null);
+        callback(null, data);
       }
     });
   } else {
-    callback('');
+    callback('Employer must be logged in to GET posted jobs.');
   }
 };
 
