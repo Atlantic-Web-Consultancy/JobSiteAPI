691d34bb98f76813ef7ee1f9f4449ee26bf30b86
Samuel Gasser <sgasser94@gmail.com>
2021-05-25 12:51:48 -0700

    Completed GET to /seekers

--- model/seeker/getSeeker.js
+++ model/seeker/getSeeker.js
@@ -1,7 +1,36 @@
 const client = require('../../database/pg.js');
 const utils = require('../../lib/hashUtils.js');
 
-getSeeker = (data, callback) => {
+getSeeker = (cookie, callback) => {
+  const sessionQueryString = `SELECT user_id FROM session WHERE cookie = $1`;
+  const sessionQueryValues = [cookie];
+  const sessionQuery = {
+    text: sessionQueryString,
+    values: sessionQueryValues
+  }
+  client.query(sessionQuery)
+  .then((sessionData) => {
+    const userId = sessionData.rows[0].user_id;
+    if (isNaN(userId)) {
+      throw('Invalid user_id, something went wrong.');
+    }
+    console.log('userid', userId);
+    const applicantQueryString = `SELECT * FROM applicants WHERE id = $1`;
+    const applicantQueryValues = [userId];
+    const applicantQuery = {
+      text: applicantQueryString,
+      values: applicantQueryValues
+    }
+    return client.query(applicantQuery)
+  })
+  .then((applicantData) => {
+    console.log('applicantdata', applicantData.rows[0]);
+    callback(null, applicantData.rows[0]);
+  })
+  .catch((err) => {
+    console.log('err in catch block', err);
+    callback(err);
+  })
 };
 
 module.exports = getSeeker;
