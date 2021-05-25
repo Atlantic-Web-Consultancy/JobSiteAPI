fcc772dcba69682b288049711fefab6c9ba58d57
Samuel Gasser <sgasser94@gmail.com>
2021-05-25 13:12:27 -0700

    Completed GET to /employers

--- controllers/employer/getEmployer.js
+++ controllers/employer/getEmployer.js
@@ -1,16 +1,17 @@
 const Model = require('../../model/');
 
-const getEmployer = (data, callback) => {
-  if (Object.keys(data).length === 7) {
-    Model.Employer.getEmployer(data, (err) => {
+const getEmployer = (req, callback) => {
+  var jobSiteCookie = req.cookies.jobsite;
+  if (jobSiteCookie) {
+    Model.Employer.getEmployer(jobSiteCookie, (err, data) => {
       if (err) {
         callback(err);
       } else {
-        callback(null);
+        callback(null, data);
       }
     });
   } else {
-    callback('');
+    callback('Employer must be logged in to GET employer information.');
   }
 };
 
