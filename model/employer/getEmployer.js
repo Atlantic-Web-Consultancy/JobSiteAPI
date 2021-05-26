d757407fae8f41531ae51a85bd018b240173d907
Samuel Gasser <sgasser94@gmail.com>
2021-05-26 09:03:57 -0700

    Changed template literals to single quotes

--- model/employer/getEmployer.js
+++ model/employer/getEmployer.js
@@ -2,7 +2,7 @@ const client = require('../../database/pg.js');
 const utils = require('../../lib/hashUtils.js');
 
 getEmployer = (cookie, callback) => {
-  const sessionQueryString = `SELECT user_id FROM session WHERE cookie = $1`;
+  const sessionQueryString = 'SELECT user_id FROM session WHERE cookie = $1';
   const sessionQueryValues = [cookie];
   const sessionQuery = {
     text: sessionQueryString,
@@ -14,7 +14,7 @@ getEmployer = (cookie, callback) => {
     if (isNaN(userId)) {
       throw('Invalid user_id, something went wrong.');
     }
-    const employerQueryString = `SELECT * FROM employers WHERE id = $1`;
+    const employerQueryString = 'SELECT * FROM employers WHERE id = $1';
     const employerQueryValues = [userId];
     const employerQuery = {
       text: employerQueryString,
