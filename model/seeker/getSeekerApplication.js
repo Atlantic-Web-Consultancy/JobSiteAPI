40a1420407bf1f4ae3eed8f4f46db3185596b8c8
Samuel Gasser <sgasser94@gmail.com>
2021-05-26 10:44:51 -0700

    Compelted seekers/applications route, does not send documents, just id numbers of documents.

--- model/seeker/getSeekerApplication.js
+++ model/seeker/getSeekerApplication.js
@@ -23,40 +23,7 @@ getSeekerApplication = (cookie, callback) => {
       return client.query(applicationsQuery);
     })
     .then((applicationsData) => {
-      console.log(applicationsData.rows);
-
-      //
-      //
-      //
-      // iterate through applications data
-      // push every resume and cover letter id to documents array
-      // query documents db for every id in documents array
-      const documentsQueryString = 'SELECT * FROM documents WHERE id = ANY(ARRAY$1)';
-      const documentIds = [];
-      for (let i = 0; i < applicationsData.length; i++) {
-        documentIds.push(applicationsData[i].cover);
-        documentIds.push(applicationsData[i].resume);
-      }
-      documentsQuery = {
-        text: documentsQueryString,
-        values: [documentIds]
-      };
-      return client.query(documentsQuery);
-    })
-    .then((documentsData) => {
-    // put applications data and documents data into response data object
-      const docsAndAppsObject = {
-        applications: applicationsData.rows,
-        documents: documentsData.rows
-      };
-      //
-      //
-      //
-      //
-      //
-
-      console.log('docsAndAppsObject', docsAndAppsObject);
-      callback(null, docsAndAppsObject);
+      callback(null, applicationsData);
     })
     .catch((err) => {
       callback(err);
