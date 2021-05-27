ff2f45eccee2e9bfe34f13a3589a6791fdfcc6a1
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-27 11:22:07 -0700

    Fix Create Jobs

--- model/general/createJob.js
+++ model/general/createJob.js
@@ -10,8 +10,8 @@ const createJob = (body, cookie, callback) => {
     .then((sessionQueryData) => {
       const userId = sessionQueryData.rows[0].user_id;
       const jobPostingQuery = {
-        text: 'INSERT INTO job_postings (title, description, employment_type, experience_level, salary, type_work, date_posted, location) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-        values: [body.title, body.description, body.employment_type, body.experience_level, body.salary, body.type_work, Date.now(), body.location]
+        text: 'INSERT INTO job_postings (title, description, employer_id, employment_type, experience_level, salary, type_work, date_posted, location) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+        values: [body.title, body.description, userId, body.employment_type, body.experience_level, body.salary, body.type_work, Date.now(), body.location]
       };
       return client.query(jobPostingQuery);
     })
